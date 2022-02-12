2bb415d51f993e287b47038decf035fc391df255
lianguili <liliangui369@163.com>
2022-02-12 22:28:07 +0800

    multi-platform

---
+++ agui_sh/aguidemo_demos_git_push_all.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+echo "============================================="
+echo "aguidemo_demos_git_push_all"
+echo "Li  Liangui"
+echo "liliangui369@163.com"
+echo "https://github.com/liliangui"
+echo "============================================="
+
+#vars
+commitstr="multi-platform"
+remotestr="origin"
+#remotestr="osc master"
+#remotestr="github master"
+#remotestr="origin"
+
+#git push
+cd gittest
+git add --all .
+git commit -m ${commitstr}
+git push ${remotestr} master
+cd ..
+
